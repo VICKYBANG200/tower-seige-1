4d418ef058a654f5483b930b3551a61698f30a8e
VICKYBANG200 <71639939+VICKYBANG200@users.noreply.github.com>
2020-11-02 14:58:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var block1,block2,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16;
+
+var ground;
+
+function setup() {
+  createCanvas(00,400);
+  
+  ground = new Ground(450,390,900,20);
+
+  block1 = new Box(300,275,30,40);
+  block2 = new Box(330,275,30,40);
+  block3 = new Box(360,275,30,40);
+  block4 = new Box(390,275,30,40);
+  block5 = new Box(420,275,30,40);
+  block6 = new Box(450,275,30,40);
+  block7 = new Box(480,275,30,40);
+  block8 = new Box(330,235,30,40);
+  block9 = new Box(360,235,30,40);
+  block10 = new Box(390,235,30,40);
+  block11 = new Box(420,235,30,40);
+  block12 = new Box(450,235,30,40);
+  block13 = new Box(360,195,30,40);
+  block14 = new Box(390,195,30,40);
+  block15 = new Box(420,195,30,40);
+  block16 = new Box(390,155,30,40);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  fill("orange");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("red");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("black");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+
+
+  drawSprites();
+}
